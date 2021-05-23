54f8b05089d505f3f7f1093599ece4f6dfb11532
WorkAttackDev <workattackgeral@gmail.com>
2021-05-23 22:30:06 +0100

    2 files modified

--- pages/invoice.tsx
+++ pages/invoice.tsx
@@ -1,11 +1,15 @@
+import InvoiceHeader from "../components/invoice/InvoiceHeader";
+
 type Props = {};
+
+
 
 const InvoicePage:React.FC<Props> = () => {
 
 return ( 
 <main className="min-h-screen flex justify-center items-center bg-gray-200 p-12">
-      <div className="w-[210mm] h-[297mm] p-12 bg-white shadow">
-        Invoice
+      <div className="w-[210mm] h-[297mm]  bg-white shadow">
+        <InvoiceHeader />
       </div>
  </main>); 
 
