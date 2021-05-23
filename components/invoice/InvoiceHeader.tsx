01b1b142d367c8b96078da0aa8fab0deb96689c8
WorkAttackDev <workattackgeral@gmail.com>
2021-05-23 22:49:35 +0100

    1 file modified

--- components/invoice/InvoiceHeader.tsx
+++ components/invoice/InvoiceHeader.tsx
@@ -2,7 +2,7 @@ type Props = {};
 
 const headerData = [
   {title: "Código da proforma", text: "WAP001"},
-  {title: "Data de emissão", text: new Date().toString()},
+  {title: "Data de emissão", text: "Sun May 23 2021 22:48:41 GMT+0100 (Horário Padrão da África Ocidental)"},
 ];
 
 const InvoiceHeader:React.FC<Props> = () => {
@@ -16,7 +16,7 @@ return (
       <h1 className="font-bold text-2xl mb-8">Proforma</h1>
       {
         headerData.map(({title, text}) => (
-        <span className="first:mb-4">
+        <span key={title} className="first:mb-4">
           <strong>{title}</strong>
           <p className="text-opacity-20">{text}</p>
         </span>
