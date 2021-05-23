922849cfec251b8f88984ec86199cad8af405a1d
WorkAttackDev <workattackgeral@gmail.com>
2021-05-23 21:51:51 +0100

    1 file modified

--- pages/invoice.tsx
+++ pages/invoice.tsx
@@ -2,8 +2,11 @@ type Props = {};
 
 const InvoicePage:React.FC<Props> = () => {
 
-return ( <main className="w-[210] h-[297]">
-  Invoice
+return ( 
+<main className="min-h-screen flex justify-center items-center bg-gray-200 p-12">
+      <div className="w-[210mm] h-[297mm] p-12 bg-white shadow">
+        Invoice
+      </div>
  </main>); 
 
 }
